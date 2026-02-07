f0ec7ca5e79a2ffdf7c69fae27a3e88e2023c10e
Jaysen Lestari <lestarijaysen@gmail.com>
2026-02-07 12:31:02 +0700

    fix product view rendering

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -16,7 +16,7 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-    @GetMapping("/Create")
+    @GetMapping("/create")
     public String createProductPage(Model model){
         Product product = new Product();
         model.addAttribute("product",product);
